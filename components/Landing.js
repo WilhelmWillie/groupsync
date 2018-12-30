f943f299fbd178efbb89e9f9163d0ed2f3fd3444
Wilhelm Willie <wilhelmwillie@gmail.com>
2018-12-30 13:39:31 -0500

    Basic Bulma set up and config. Will prob re-do

--- components/Landing.js
+++ components/Landing.js
@@ -1,10 +1,23 @@
+import style from '../styles/styles.scss'
+
 const Landing = () => {
   return (
     <div>
-      <h1>GroupSync!</h1>
+      <style dangerouslySetInnerHTML={{ __html: style }} />
 
-      <button>Create</button>
-      <button>Join</button>
+      <div className='container has-text-centered' style={{padding: '5em 0'}}>
+        <h1 className='title is-1'>GroupSync!</h1>
+
+        <div className='field is-grouped' style={{justifyContent: 'center'}}>
+          <p className='control'>
+            <a className='button is-primary'>Create</a>
+          </p>
+
+          <p className='control'>
+            <a className='button is-primary'>Join</a>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
